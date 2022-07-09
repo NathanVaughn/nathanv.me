8e339027f588f5c430718f2304c49166b9ba4ba4
Nathan Vaughn <nvaughn51@gmail.com>
2022-07-08 22:24:49 -0500

    Try using my own package registry

--- postprocess.js
+++ postprocess.js
@@ -1,22 +1,22 @@
-import assert from "assert";
-import { copyFileSync } from "fs";
-import { sync } from "glob";
-import { basename, join, dirname } from "path";
+const assert = require("assert");
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
 
 const full_regex = new RegExp('^.+\.[0-9a-f]{8}\..+$');
 const sub_regex = new RegExp('[0-9a-f]{8}\.');
 
 function main() {
     // iterate through every file in the dist directory
-    sync("dist/**/*").forEach((file) => {
-        file_basname = basename(file);
+    glob.sync("dist/**/*").forEach((file) => {
+        file_basname = path.basename(file);
         if (full_regex.test(file_basname)) {
             // if the file name matches the regex, then copy it
             new_file_basname = file_basname.replace(sub_regex, "");
-            new_file = join(dirname(file), new_file_basname);
+            new_file = path.join(path.dirname(file), new_file_basname);
 
             console.log(`Copying ${file} to ${new_file}`);
-            copyFileSync(file, new_file);
+            fs.copyFileSync(file, new_file);
         }
     });
 }
