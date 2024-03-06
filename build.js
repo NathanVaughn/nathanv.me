74a66f6a5ed7db5048fff2047dd267b684d65c3c
Nathan Vaughn <nvaughn51@gmail.com>
2024-03-05 19:45:37 -0600

    Add favicon

--- build.js
+++ build.js
@@ -1,20 +1,39 @@
-const spawn = require('cross-spawn');
-var base_url = "/";
+const spawn = require("cross-spawn");
+const toml = require("@iarna/toml");
+const fs = require("fs");
+const path = require("path");
+const pngToIco = require("png-to-ico");
 
-if (process.env.CF_PAGES_BRANCH === 'main') {
-    base_url = "https://nathanv.me/";
+let base_url = "/";
+
+if (process.env.CF_PAGES_BRANCH === "main") {
+  base_url = "https://nathanv.me/";
 } else if (process.env.CF_PAGES_URL) {
-    base_url = process.env.CF_PAGES_URL;
+  base_url = process.env.CF_PAGES_URL;
 }
 
 console.log(`Using base url "${base_url}"`);
-cmd = spawn.sync("npx", ["hugo", "--cleanDestinationDir", "--minify", "-b", base_url], { encoding : 'utf8' });
+cmd = spawn.sync(
+  "npx",
+  ["hugo", "--cleanDestinationDir", "--minify", "-b", base_url],
+  { encoding: "utf8" }
+);
 
 if (cmd.error) {
-    console.log("ERROR: ", cmd.error);
+  console.log("ERROR: ", cmd.error);
+  process.exit(cmd.status);
 }
 
 console.log(cmd.stdout);
 console.error(cmd.stderr);
 
-process.exit(cmd.status);
+// make a favicon at the root directory
+let hugo_config = toml.parse(fs.readFileSync("hugo.toml"));
+let color = hugo_config.params.themeColor;
+let png_file = path.join("public", "img", "theme-colors", `${color}.png`);
+
+pngToIco(png_file)
+  .then((buf) => {
+    fs.writeFileSync(path.join("public", "favicon.ico"), buf);
+  })
+  .catch(console.error);
